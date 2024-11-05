d8d355e1f55eea7278b5b8160cd46dc59bd96f45
MarcoPoloGe <92299431+MarcoPoloGe@users.noreply.github.com>
2024-11-05 16:15:29 +0100

    added shell scripts for installation and operation

---
+++ install/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# setup of the label-bot working environment
+sudo apt update
+sudo apt install -y vim git python3-full python3-pip screen libpangocairo-1.0-0
+python3 -m venv .venv
+source .venv/bin/activate
+pip3 install -r requirements.txt
