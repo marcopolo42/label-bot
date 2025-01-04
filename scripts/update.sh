8891670255d683a42d2d2b33625fa29dce77a9b7
MarcoPoloGe <92299431+MarcoPoloGe@users.noreply.github.com>
2025-01-04 22:34:11 +0100

    - update.sh now only installs requirements if necessary.

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,12 +3,31 @@
 # Navigate to the project directory
 cd /home/admin/label-bot || exit
 
+# File paths
+REQUIREMENTS_FILE="requirements.txt"
+
+# Compute checksum before Git pull
+PRE_PULL_CHECKSUM=$(md5sum $REQUIREMENTS_FILE | awk '{print $1}' 2>/dev/null)
+
 # Pull the latest changes from the main branch
 git pull origin main
 
-sudo .venv/bin/pip3 install -r requirements.txt
+# Compute checksum after Git pull
+POST_PULL_CHECKSUM=$(md5sum $REQUIREMENTS_FILE | awk '{print $1}')
 
-# Restart the service (replace 'your-service' with the actual service name)
+# Check if the requirements.txt file has changed
+if [ "$PRE_PULL_CHECKSUM" != "$POST_PULL_CHECKSUM" ]; then
+    echo "Detected changes in $REQUIREMENTS_FILE. Installing requirements..."
+
+    # Install updated requirements
+    sudo .venv/bin/pip3 install -r $REQUIREMENTS_FILE
+
+    echo "Requirements updated successfully."
+else
+    echo "No changes detected in $REQUIREMENTS_FILE. Skipping requirements installation."
+fi
+
+# Restart the service
 sudo systemctl restart labelbot.service
 
 echo "Project updated and service restarted successfully."
