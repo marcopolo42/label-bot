ebec8f6d40c41afec1fff5327bc4b4610a877b75
MarcoPoloGe <92299431+MarcoPoloGe@users.noreply.github.com>
2025-04-16 18:24:48 +0200

    - the price icon in the previews now scales with the ratio of the image so that is allways readable in discord.

--- label_cog/src/coins_render.py
+++ label_cog/src/coins_render.py
@@ -20,13 +20,31 @@ def render_coins_image(coins):
 
 
 def add_price_icon(img, price):
-    img_price = render_coins_image(price)
-    img_price = add_margins(img_price, (0, 0, 0, 0), dpi=300, color=(0, 0, 0, 0))
-    # allaways positive ratio
-    ratio = abs(img.width / img_price.width)
+    if price <= 0:
+        return img
+    if img.width != img.height:
+        logger.warning("Coin image is not square, this may cause issues")
 
-    position = (img.width - img_price.width, 0)
+    base_pixel_increase = -3
+    ratio_divisor = 1
+    offset = -160
+
+    img_price = render_coins_image(price)
+    #img_price = add_margins(img_price, (0, 0, 0, 0), dpi=300, color=(0, 0, 0, 0))
+    ratio = abs(((img.width / img.height) / ratio_divisor) - 1)
+    logger.debug(f"ratio: {ratio}")
     #because the original image is only grayscale we need to convert it to RGB so that the coin is colored after pasting
     img = img.convert("RGB")
+    #scale the coin image to fit the label
+    if ratio > 4:
+        base_pixel_increase = 0
+    if ratio > 5:
+        base_pixel_increase = 5
+    if ratio > 6:
+        base_pixel_increase = 10
+    pixel_change = int((base_pixel_increase * ratio) + offset)
+    coin_size = img_price.width + pixel_change
+    img_price = img_price.resize((coin_size, coin_size))
+    position = (img.width - img_price.width, 0)
     img.paste(img_price, position, img_price)
     return img
